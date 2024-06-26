3ff0ccb1755757e9ca4dce91dcc561c93b024cc0
Hiren Arvind Kachhadia <hirenarvind2013@gmail.com>
2024-06-26 11:55:22 +0530

    Encapsulation example

---
+++ src/Encapsulated/Person.java
@@ -0,0 +1,45 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Please enter a valid age.");
+        }
+    }
+
+    public void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("John", 25);
+
+        person.display();
+        person.setName("Doe");
+        person.setAge(30);
+        person.display();
+        
+        person.setAge(-5);
+        person.display();
+    }
+}
